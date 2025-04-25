d9fea09ede883d6a24dd586c2adb71c32d988ed4
Gia Huy <huymax100720@gmail.com>
2025-04-25 16:19:22 +0700

    init

---
+++ ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs
+{
+    public class ChatHub : Hub
+    { 
+        public async Task JoinChat(UserConnection user)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, user.ChatRoom);
+            await Clients.All.SendAsync("ReceiveMessage", $"{user.Username} has joined {user.ChatRoom}");
+        }
+
+        public async Task JoinSpecifyChatRoom(UserConnection user)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId , user.ChatRoom);
+            await Clients.Group(user.ChatRoom).SendAsync("ReceiveMessage", $"{user.Username} has joined {user.ChatRoom}");
+        }
+    }
+}
